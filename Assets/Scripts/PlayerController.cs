1f000ff7406c7e87507f3b256e87bfe85e9f6e52
Yunsik Han <yunsikhan.yh@gmail.com>
2024-07-01 13:02:12 +1200

    Added alt input system code for movement

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,25 +6,24 @@ using UnityEngine.UIElements;
 
 public class NewBehaviourScript : MonoBehaviour
 {
-    public InputAction LeftAction;
     public InputAction MoveAction;
 
     // Start is called before the first frame update
     void Start()
     {
-        LeftAction.Enable();
         MoveAction.Enable();
     }
 
     // Update is called once per frame
     void Update()
     {
-        /*
         Vector2 move = MoveAction.ReadValue<Vector2>();
         Debug.Log(move);
+        //deltatime helps set things be fps independent
         Vector2 position = (Vector2)transform.position + move * 3.0f * Time.deltaTime;
         transform.position = position;
-        */
+
+        /*
         float horizontal = 0.0f;
         if (Keyboard.current.leftArrowKey.isPressed)
         {
@@ -51,5 +50,6 @@ public class NewBehaviourScript : MonoBehaviour
         position.x = position.x + 0.1f * horizontal;
         position.y = position.y + 0.1f * vertical;
         transform.position = position;
+        */
     }
 }
