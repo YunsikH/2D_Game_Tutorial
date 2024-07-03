ee62139c7c68d9f38c8bb7c4114c8848cd75c8d5
Yunsik Han <yunsikhan.yh@gmail.com>
2024-07-03 12:40:33 +1200

    New Prefabs with collision detection

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,21 +7,29 @@ using UnityEngine.UIElements;
 public class NewBehaviourScript : MonoBehaviour
 {
     public InputAction MoveAction;
+    Rigidbody2D rigidbody2d;
+    Vector2 move;
 
     // Start is called before the first frame update
     void Start()
     {
         MoveAction.Enable();
+        rigidbody2d = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        move = MoveAction.ReadValue<Vector2>();
+        Debug.Log(move);
+
+        /*
         Vector2 move = MoveAction.ReadValue<Vector2>();
         Debug.Log(move);
         //deltatime helps set things be fps independent
         Vector2 position = (Vector2)transform.position + move * 3.0f * Time.deltaTime;
         transform.position = position;
+        */
 
         /*
         float horizontal = 0.0f;
@@ -51,5 +59,10 @@ public class NewBehaviourScript : MonoBehaviour
         position.y = position.y + 0.1f * vertical;
         transform.position = position;
         */
+    }
+    void FixedUpdate()
+    {
+        Vector2 position = (Vector2)rigidbody2d.position + move * 3.0f * Time.deltaTime;
+        rigidbody2d.MovePosition(position);
     }
 }
